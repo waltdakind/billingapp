fcbbf93b3281bb4431ddbaec783a831147818853
Walter Mattingly <waltdakind@gmail.com>
2016-07-09 13:45:36 -0400

    log in form

--- client/js/login.jsx
+++ client/js/login.jsx
@@ -4,7 +4,31 @@ var Login = React.createClass({
   render: function () {
     return (
       <div><h2>Welcome to Our Billing App</h2>
-       <h3> This is the log in page. </h3>
+       <div class="large-3 large-centered columns">
+  <div class="login-box">
+  <div class="row">
+  <div class="large-12 columns">
+    <form>
+       <div class="row">
+         <div class="large-12 columns">
+             <input type="text" name="username" placeholder="Username" />
+         </div>
+       </div>
+      <div class="row">
+         <div class="large-12 columns">
+             <input type="password" name="password" placeholder="Password" />
+         </div>
+      </div>
+      <div class="row">
+        <div class="large-12 large-centered columns">
+          <input type="submit" class="button expand" value="Log In"/>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
+</div>
        </div>
     );
   }
